25d25992351ffabe0d37819948a00a9e7d27b982
FranziMe <f.metge88@gmail.com>
2020-05-18 12:04:53 +0200

    week1 push notebook

---
+++ week1.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_markers: '{{{,}}}'
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# ## This jupyter notebook will be used to report analysis and results for the Coursera Capstone project
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
